da12b84524475919bc3784e9e9a436f82c5fbc8d
KobzarMax <nimbus2219@gmail.com>
2023-04-26 19:00:48 +0200

    videos autoplay

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const videos = document.querySelectorAll("#videoTeaser4");
-
-videos.forEach((video) => {
-    if (video.paused) {
-      video.play();  
-    } else {
-      video.pause();
-    }
-})
